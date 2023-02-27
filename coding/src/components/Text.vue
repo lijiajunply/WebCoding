efef1f41de21a926eece9a6eaa9cdc79ca1871b6
LuckyFish <2383690354@qq.com>
2023-02-27 21:52:44 +0800

    finish

--- coding/src/components/Text.vue
+++ coding/src/components/Text.vue
@@ -1,11 +1,21 @@
 <template>
-  <!-- :indent-with-tab="true" 是否自动获取焦点-->
+
+  <div id="app">
+    <label>C++<input type="checkbox" v-model="gender" value="C++"/></label>
+    <label>CSharp<input type="checkbox" v-model="gender" value="CSharp"/></label>
+    <label>Java<input type="checkbox" v-model="gender" value="Java"/></label>
+    <label>Python<input type="checkbox" v-model="gender" value="Python"/></label>
+    <label>C<input type="checkbox" v-model="gender" value="C"/></label>
+    <label>Python2<input type="checkbox" v-model="gender" value="Python"/></label>
+  </div>
+
   <codemirror v-model="code"
-              placeholder="Code gose here..."
               :style="{ height: '100%' }"
               :autofocus="true"
               :tabSize="2"
               :extensions="extensions"/>
+  <button @click="f()" >Run</button>
+  <label>{{result}}</label>
 </template>
 <script lang="ts" setup>
 import { Codemirror } from "vue-codemirror";
@@ -13,16 +23,25 @@ import { javascript } from "@codemirror/lang-javascript";
 import { oneDark } from "@codemirror/theme-one-dark";
 import { ref } from "vue";
 import {java} from "@codemirror/lang-java";
-interface IProps {
-  height?: string,
-}
-// 接受的参数
-const props = withDefaults(defineProps<IProps>(), {
-  height: '400px'
-})
+import {cpp} from "@codemirror/lang-cpp";
+import Axios from "axios";
+let gender = "";
+let result = "";
+console.log(gender);
 const code = ref(``);
-const extensions = [java(), oneDark];
-const Change = () => {
+const extensions = [javascript(), oneDark];
 
+function f() {
+  window.axios.post("https://localhost:7045/api/Debug",window.qs.stringify({
+    'code' : code,
+    'lang' : gender}
+  )).then(function (res) {
+    if (res.data.code == 200) {
+      result = res.data.data;
+    }
+  }).catch(function (error) {
+    console.log(error);
+  });
 }
+
 </script>
--- coding/src/main.js
+++ coding/src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-
 createApp(App).mount('#app')
+
+import Axios from "axios"
+import qs from 'qs';
+window.qs = qs
+window.axios=Axios
