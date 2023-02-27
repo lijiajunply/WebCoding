645f1f13ce286c3c5a5322948405f153292194b4
LuckyFish <2383690354@qq.com>
2023-02-27 15:26:48 +0800

    add WebCoding.App

---
+++ coding/src/components/Text.vue
@@ -0,0 +1,28 @@
+<template>
+  <!-- :indent-with-tab="true" 是否自动获取焦点-->
+  <codemirror v-model="code"
+              placeholder="Code gose here..."
+              :style="{ height: '100%' }"
+              :autofocus="true"
+              :tabSize="2"
+              :extensions="extensions"/>
+</template>
+<script lang="ts" setup>
+import { Codemirror } from "vue-codemirror";
+import { javascript } from "@codemirror/lang-javascript";
+import { oneDark } from "@codemirror/theme-one-dark";
+import { ref } from "vue";
+import {java} from "@codemirror/lang-java";
+interface IProps {
+  height?: string,
+}
+// 接受的参数
+const props = withDefaults(defineProps<IProps>(), {
+  height: '400px'
+})
+const code = ref(``);
+const extensions = [java(), oneDark];
+const Change = () => {
+
+}
+</script>
