a81df340681bb5db49ef7cdd65ffe3a611b073f1
李嘉俊 <2383690354@qq.com>
2024-04-16 01:10:21 +0800

    Aot 关闭

---
+++ Test/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+internal record CodeModel
+{
+    public string Code { get; set; } = "";
+    public string Lang { get; set; } = "";
+}
