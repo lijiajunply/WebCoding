5986c7433060578c950f994ced09def61fac46f4
李嘉俊 <2383690354@qq.com>
2024-04-16 10:36:43 +0800

    dotnet环境

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,6 +1,17 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Net.Http.Json;
+
 Console.WriteLine("Hello, World!");
+
+using var client = new HttpClient();
+var response = await client.PostAsJsonAsync("https://code.zeabur.app/", new CodeModel
+{
+    Code = "def main():\n    print(\"Hello, World!\")\n",
+    Lang = "py"
+});
+
+Console.WriteLine(await response.Content.ReadAsStringAsync());
 
 internal record CodeModel
 {
